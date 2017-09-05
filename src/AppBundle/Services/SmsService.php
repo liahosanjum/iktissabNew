5baa4579f2f1e9e0af2f68ecfad460cfd96b174e
Salem Aman <salemkhan2001@gmail.com>
2017-09-05 11:13:22 +0300

    Add FOPEN support for json services

--- src/AppBundle/Services/SmsService.php
+++ src/AppBundle/Services/SmsService.php
@@ -10,25 +10,25 @@ namespace AppBundle\Services;
 
 
 use AppBundle\AppConstant;
-use Symfony\Component\Config\Definition\Exception\Exception;
+
 
 class SmsService
 {
     private $params;
     private $restClient;
 
-    public function __construct(RestClientService $restClientService, $params)
+    public function __construct(FOpenWrapper $restClient, $params)
     {
         $this->params = $params;
-        $this->restClient = $restClientService;
+        $this->restClient = $restClient;
     }
 
     function sendSms($receiver, $message, $country)
     {
         if ($country == 'eg')
         {
-            $mobilyUser = $this->params['mobily_user_eg'];
-            $mobilyPass = $this->params['mobily_pass_eg'];
+            $user = $this->params['mobily_user_eg'];
+            $pass = $this->params['mobily_pass_eg'];
             // for EG we store the countrycode in with mobile number
             // so no need to add prefix
             $countryPrefix = '';  //AppConstant::IKT_EG_PREFIX;
@@ -37,42 +37,34 @@ class SmsService
             }
 
 
-            $mobilySender = $this->params['mobily_sender_eg'];
+            $sender = $this->params['mobily_sender_eg'];
         }
         else
         {
-            $mobilyUser = $this->params['mobily_user'];
-            $mobilyPass = $this->params['mobily_pass'];
+            $user = $this->params['mobily_user'];
+            $pass = $this->params['mobily_pass'];
             $countryPrefix = AppConstant::IKT_SA_PREFIX;
-            $mobilySender = $this->params['mobily_sender'];
+            $sender = $this->params['mobily_sender'];
             // format number
             if(strlen($receiver) == 10)
                 $receiver = substr($receiver,1 ,strlen($receiver)-1);
         }
-        // for testing
-        // $receiver_mob = $receiver;
-        //
+
         $msgID  = rand(1, 9999);
         $delKey = rand(1, 9999);
-        //iconv ( "UTF-8", "windows-1256", $message );
-        //$messageFormatted = urlencode(iconv("UTF-8", "windows-1256", $message));
+
         $messageFormatted = urlencode($message);
 
-        $payload = "mobile=" . $mobilyUser . "&password=" . $mobilyPass . "&numbers=" . $countryPrefix . $receiver . "&sender=" . $mobilySender . "&msg=" . $messageFormatted . "&timeSend=0&dateSend=0&applicationType=" . $this->params['mobily_app_type'] . "&domainName=" . $this->params['mobily_app_type'] . "&msgId=" . $msgID . "&deleteKey=" . $delKey . "&lang=3";
+        $payload = "mobile=" . $user . "&password=" . $pass . "&numbers=" . $countryPrefix . $receiver . "&sender=" . $sender . "&msg=" . $messageFormatted . "&timeSend=0&dateSend=0&applicationType=" . $this->params['mobily_app_type'] . "&domainName=" . $this->params['mobily_app_type'] . "&msgId=" . $msgID . "&deleteKey=" . $delKey . "&lang=3";
         $url = 'http://www.mobily.ws/api/msgSend.php?' . $payload;
         //echo $url;
-        $sms = $this->restClient->restGet($url, array());
+        $sms = $this->restClient->get($url, ["Content-Type"=>"text\html"]);
 //        $sms = 1;
-       // var_dump($sms);
-//        die('---');
+
         if ($sms == '1') {
             return true;
         }
-        /*
-        else {
-            Throw new Exception('Failed to send sms');
-        }
-        */
+
         return false;
     }
 
@@ -108,7 +100,7 @@ class SmsService
         $payload = "mobile=" . $mobilyUser . "&password=" . $mobilyPass . "&numbers=" . $countryPrefix . $receiver . "&sender=" . $mobilySender . "&msg=" . $messageFormatted . "&timeSend=0&dateSend=0&applicationType=" . $this->params['mobily_app_type'] . "&domainName=" . $this->params['mobily_app_type'] . "&msgId=" . $msgID . "&deleteKey=" . $delKey . "&lang=3";
         $url = 'http://www.mobily.ws/api/msgSend.php?' . $payload;
         //echo $url;
-        $sms = $this->restClient->restGet($url, array());
+        $sms = $this->restClient->get($url, ["Contenttype"=>"text\html"]);
 //        $sms = 1;
         // var_dump($sms);
 //        die('---');
@@ -145,7 +137,7 @@ class SmsService
                 $receiver = substr($receiver,1 ,strlen($receiver)-1);
         }
         // for testing
-        $receiver_mob = $receiver;
+
         $msgID  = rand(1, 9999);
         $delKey = rand(1, 9999);
         //$messageFormatted = urlencode(iconv("UTF-8", "windows-1256", $message));
@@ -155,7 +147,7 @@ class SmsService
         $payload = "mobile=" . $mobilyUser . "&password=" . $mobilyPass . "&numbers=" . $countryPrefix . $receiver . "&sender=" . $mobilySender . "&msg=" . $messageFormatted . "&timeSend=0&dateSend=0&applicationType=" . $this->params['mobily_app_type'] . "&domainName=" . $this->params['mobily_app_type'] . "&msgId=" . $msgID . "&deleteKey=" . $delKey . "&lang=3";
         $url = 'http://www.mobily.ws/api/msgSend.php?' . $payload;
         //echo $url;
-        $sms = $this->restClient->restGet($url, array());
+        $sms = $this->restClient->get($url, ["Content-Type"=>"text\html"]);
 //        $sms = 1;
         // var_dump($sms);
 //        die('---');
